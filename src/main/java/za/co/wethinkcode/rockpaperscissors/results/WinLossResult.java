1f7dc942eaa1c74ccd450e45577d74ce32deecaa
keanaido021 <keanaido021@student.wethinkcode.co.za>
2022-03-30 15:12:04 +0200

    Might be done

--- src/main/java/za/co/wethinkcode/rockpaperscissors/results/WinLossResult.java
+++ src/main/java/za/co/wethinkcode/rockpaperscissors/results/WinLossResult.java
@@ -16,13 +16,31 @@ public class WinLossResult extends GameResult{
     @Override
     public String toString() {
         List<String> output = new ArrayList<>() {};
+        List<String> winners = new ArrayList<>();
+        int winningScore = 0;
         int i = 0;
         for (Player player : players()) {
-            output.add("(Player " + i + ") Won: " + player.getWins() + ", " +
-                    "Lost: " + (player.opponents().size() - player.getWins()));
+            int score = player.getWins();
+            int lost = 0;
+
+            for (Player opponent: player.opponents()) {
+                if (player.getHand().compareTo(opponent.getHand()) < 0) lost++;
+            }
+
+            output.add("(Player " + i + ") Won: " + score + ", Lost: " + lost);
+            if (score > winningScore) winningScore = score;
             i++;
         }
 
-        return String.join("\n", output);
+        i = 0;
+        for (Player player : players()) {
+            int score = player.getWins();
+            if (score == winningScore) {
+                winners.add("(Player " + i + ")");
+            }
+            i++;
+        }
+        return String.join("\n", output) + "\n\n" +
+                String.join(", ", winners) + " Won!";
     }
 }
